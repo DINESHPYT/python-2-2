e38cdf9ef48b37fae2e03bf9ca58b457ff6c0572
DINESHPYT <153349987+DINESHPYT@users.noreply.github.com>
2023-12-26 15:05:48 +0530

    Add files via upload

---
+++ DINESH PY FOR YOU.py
@@ -0,0 +1,23 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+a = int (input('enter the number:'))
+b = int (input('enter the another number:'))
+print(a%b)
+print('DINEH''\n'*3)
+    
+print((a>b) or (a==6))
+
+
+if (a<b):
+    print('ok statement')    # TODO: write code...
+elif (a==22):
+    print('between')
+else:
+    print('not ok statement')
+    
+    
